6e960bea2c4da6d64ee8baf875a45b8c9badb9a0
Your Name <you@example.com>
2020-12-07 17:49:23 +0200

    email and username validation

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,8 +25,9 @@ class LoginController extends Controller
     public function store(Request $request)
     {
         $this->validate($request, [
-            'email' => 'required|email',
+            'email' => 'required|email|unique:email',
             'password' => 'required',
+            'username' => 'required|unique:username',
         ]);
 
         if(!auth()->attempt($request->only('email', 'password'), $request->remember))
--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -25,8 +25,8 @@ class RegisterController extends Controller
     {
         $this->validate($request, [
             'name' => 'required|max:150',
-            'username' => 'required|max:50',
-            'email' => 'required|email|max:255',
+            'username' => 'required|max:50|unique:users',
+            'email' => 'required|email|max:255|unique:users',
             'password' => 'required|confirmed',
         ]);
 
