c0ff560a9ca6168a70377cb5525bdd90b9089159
Ihor <mrsettle1@gmail.com>
2021-09-13 14:52:00 +0300

    about us

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,10 +3,10 @@
 @section('content')
     <div class="flex justify-center mt-8 mb-20">
         <div class="w-8/12 bg-white p-6 rounded-lg ">
-            <h1>Intelligent Webapps for Your Business Logic</h1>
+            <h1 class="text-xl">Intelligent Webapps for Your Business Logic</h1>
             <hr>
-            <p>Hello, and welcome to the blog of passionated software developer, Ihor. If you would like to hire me as a developer or just ask for free IT cosultation you are welcome to fill contact form. And I'll get in touch with you as soon as possible. Have a nice day</p>
-            <h5>providing simplicity </h5>
+            <p class="mt-2 mb-2">Hello, and welcome to the blog of passionated software developer, Ihor. If you would like to hire me as a developer or just ask for free IT cosultation you are welcome to fill contact form. And I'll get in touch with you as soon as possible. Have a nice day</p>
+            <h5 class="test-lg">providing simplicity </h5>
             
         </div>
 
