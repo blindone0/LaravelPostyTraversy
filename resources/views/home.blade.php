06f4e7ca7073b130136721ab71f3ce0626df8b04
Ihor <mrsettle1@gmail.com>
2021-09-13 13:40:30 +0300

    overfloww clas

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -26,7 +26,7 @@
             </a>
         </div>
         <div :class="open ? 'block': 'hidden'" class="w-full flex-grow sm:flex sm:items-center sm:w-auto">
-            <div class="w-full container mx-auto flex flex-col sm:flex-row items-center justify-center text-sm font-bold uppercase mt-0 px-6 py-2">
+            <div class="w-full container mx-auto flex flex-col sm:flex-row items-center justify-center text-sm font-bold uppercase mt-0 px-6 py-2 overflow-x-auto">
                 @if($hashtags->count())
                     @foreach ($hashtags as $tag)
                         <a href="{{route('postsByHash', $tag)}}" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">{{$tag->hashtag}}</a>
