ade1a109aa472c86f5c2fca28de813b2f83deed5
Ihor <mrsettle1@gmail.com>
2021-09-13 14:38:37 +0300

    contact form styling

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -75,7 +75,7 @@
                 @endif
             </div>
         
-            <input type="submit" name="send" value="Submit" class=" m-10 flex-shrink-0 border-transparent border-4 text-teal-500 hover:text-teal-800 text-sm py-1 px-2 rounded">
+            <input type="submit" name="send" value="Submit" class="flex-shrink-0 border-transparent border-4 text-teal-500 hover:text-teal-800 text-sm py-1 px-2 rounded">
         </form>
     </div>
 
