6f42ba07b7bd6e51c09433ad0565d10e65115712
Ihor <mrsettle1@gmail.com>
2021-09-13 12:42:59 +0300

    happy gay day

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -64,8 +64,8 @@
         <aside class="w-full md:w-1/3 flex flex-col items-center px-3">
 
             <div class="w-full bg-white shadow flex flex-col my-4 p-6">
-                <p class="text-xl font-semibold pb-5">About Us</p>
-                <p class="pb-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas mattis est eu odio sagittis tristique. Vestibulum ut finibus leo. In hac habitasse platea dictumst.</p>
+                <p class="text-xl font-semibold pb-5">Abouе site</p>
+                <p class="pb-2">Hello, here you can post your articles or read already existing. Just register and start posting. Happy blogging</p>
                 <a href="#" class="w-full bg-blue-800 text-white font-bold text-sm uppercase rounded hover:bg-blue-700 flex items-center justify-center px-2 py-3 mt-4">
                     Get to know us
                 </a>
