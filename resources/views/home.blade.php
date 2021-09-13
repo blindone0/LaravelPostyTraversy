8d259b17fe774760a139f41ce88b9b323a881e5d
Ihor <mrsettle1@gmail.com>
2021-09-13 14:06:18 +0300

    contact button

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -66,7 +66,7 @@
             <div class="w-full bg-white shadow flex flex-col my-4 p-6">
                 <p class="text-xl font-semibold pb-5">Abouе site</p>
                 <p class="pb-2">Hello, here you can post your articles or read already existing. Just register and start posting. Happy blogging</p>
-                <a href="#" class="w-full bg-blue-800 text-white font-bold text-sm uppercase rounded hover:bg-blue-700 flex items-center justify-center px-2 py-3 mt-4">
+                <a href="{{route('contact.store', $tag)}}" class="w-full bg-blue-800 text-white font-bold text-sm uppercase rounded hover:bg-blue-700 flex items-center justify-center px-2 py-3 mt-4">
                     Get to know us
                 </a>
             </div>
