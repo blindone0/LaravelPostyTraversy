966654e37687c543097fe2dafc0fdde15669fb80
Ihor <mrsettle1@gmail.com>
2021-09-13 12:39:14 +0300

    Гей-дей араунд де ворлд. Хэппи бихдей оф шит

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,18 +40,7 @@
             </nav>
 
             <div class="flex items-center text-lg no-underline text-white pr-6">
-                <a class="" href="#">
-                    <i class="fab fa-facebook"></i>
-                </a>
-                <a class="pl-6" href="#">
-                    <i class="fab fa-instagram"></i>
-                </a>
-                <a class="pl-6" href="#">
-                    <i class="fab fa-twitter"></i>
-                </a>
-                <a class="pl-6" href="#">
-                    <i class="fab fa-linkedin"></i>
-                </a>
+
                 <ul class="flex items-center justify-between font-bold text-sm text-white uppercase no-underline">
                     @auth
 
