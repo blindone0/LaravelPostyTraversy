95246cc8ee8c326ea776ce2991efe51040af4723
Ihor <mrsettle1@gmail.com>
2021-09-13 12:50:47 +0300

    f15236g244yth

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -80,12 +80,9 @@
                     <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=4">
                     <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=5">
                     <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=6">
-                    <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=7">
-                    <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=8">
-                    <img class="hover:opacity-75" src="https://source.unsplash.com/collection/1346951/150x150?sig=9">
                 </div>
                 <a href="#" class="w-full bg-blue-800 text-white font-bold text-sm uppercase rounded hover:bg-blue-700 flex items-center justify-center px-2 py-3 mt-6">
-                    <i class="fab fa-instagram mr-2"></i> Follow @dgrzyb
+                    <i class="fab fa-instagram mr-2"></i> Follow @zkixcidstgbnm
                 </a>
             </div>
 
