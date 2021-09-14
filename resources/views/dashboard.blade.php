0a95760dccdc13aace277f8d1e006f5d3780c42b
Ihor <mrsettle1@gmail.com>
2021-09-14 12:57:03 +0300

    center"

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="flex justify-center mt-8 mb-20">
         <div class="w-full bg-white p-6 rounded-lg ">
-            <h1 class="text-xl">Intelligent Webapps for Your Business Logic</h1>
+            <h1 class="text-xl text-center">Intelligent Webapps for Your Business Logic</h1>
             <hr>
             <p class="mt-2 mb-2">Hello, and welcome to the blog of passionated software developer, Ihor. If you would like to hire me as a developer or just ask for free IT cosultation you are welcome to fill contact form. And I'll get in touch with you as soon as possible. Have a nice day</p>
             <h5 class="test-lg">providing simplicity </h5>
