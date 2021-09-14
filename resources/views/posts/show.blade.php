0221d8c61c6401a747abbbc68b657be1a42a0190
Ihor <mrsettle1@gmail.com>
2021-09-14 12:54:47 +0300

    w-full"

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,7 +4,7 @@
 @section('content')
 
     <div class="flex justify-center">
-        <div class="w-8/12 bg-white p-6 rounded-lg">
+        <div class="w-full bg-white p-6 rounded-lg">
             <x-singlepost :post="$post" />
         </div>
     </div>
