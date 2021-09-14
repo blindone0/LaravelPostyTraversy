2b87f19cf75333bafb50e3d598d5bb7c2049c3ed
Ihor <mrsettle1@gmail.com>
2021-09-14 12:17:42 +0300

    justify center

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,8 +8,9 @@
             <p class="mt-2 mb-2">Hello, and welcome to the blog of passionated software developer, Ihor. If you would like to hire me as a developer or just ask for free IT cosultation you are welcome to fill contact form. And I'll get in touch with you as soon as possible. Have a nice day</p>
             <h5 class="test-lg">providing simplicity </h5>
             <hr>
+            <div class="justify-center">
             <a href="{{ route('downloadFile', "cvIhorRadetskyi.docx") }}">Download my resume</a>
-            
+            </div>
         </div>
 
     </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,6 @@
 
 @section('content')
 
-        <!-- Text Header -->
     <header class="w-full container mx-auto">
         <div class="flex flex-col items-center py-12">
             <a class="font-bold text-gray-800 uppercase hover:text-gray-700 text-5xl" href="#">
