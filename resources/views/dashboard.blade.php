6d4afb497f2868b2628de8ed425f450f51785b09
Ihor <mrsettle1@gmail.com>
2021-09-14 12:59:07 +0300

    center

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,7 +6,7 @@
             <h1 class="text-xl text-center">Intelligent Webapps for Your Business Logic</h1>
             <hr>
             <p class="mt-2 mb-2">Hello, and welcome to the blog of passionated software developer, Ihor. If you would like to hire me as a developer or just ask for free IT cosultation you are welcome to fill contact form. And I'll get in touch with you as soon as possible. Have a nice day</p>
-            <h5 class="test-lg">providing simplicity </h5>
+            <h5 class="test-lg text-right">Providing Simplicity</h5>
             <hr>
             <div class="text-center">
             <a class="hover:underline shadow-inner ring-4 mt-2 border" href="{{ route('downloadFile', "cvIhorRadetskyi.docx") }}">Download my resume</a>
