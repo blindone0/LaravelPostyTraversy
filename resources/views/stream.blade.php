2cd6ce2406ebf6447f9254f9f0c64f49955ccd9e
Ihor <mrsettle1@gmail.com>
2021-09-16 16:33:00 +0300

    stream records

--- resources/views/stream.blade.php
+++ resources/views/stream.blade.php
@@ -39,7 +39,7 @@
           @foreach ($records as $record)
           <video class="video-js" controls preload="auto" width="640" height="268" 
           data-setup='{}'>
-            <source src="{{ storage_path('public/$record') }}" type="video/mp4">
+            <source src="{{ var_dump(storage_path()) }}" type="video/mp4">
           </video>
           @endforeach
         
